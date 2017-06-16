48032c5bbfc8306b5262328948a413501d45275c
Uzhinskiy Boris <lspci@mail.ru>
2017-06-17 00:26:08 +0300

    .

--- main.go
+++ main.go
@@ -1,35 +1,52 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
-    "io/ioutil"
-    "fmt"
-//    "os"
-//    "os/exec"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	//    "os"
+	//    "os/exec"
 )
 
 type test_struct struct {
-    Test string
+	Test string
 }
 
-func packer(rw http.ResponseWriter, req *http.Request) {
-    body, err := ioutil.ReadAll(req.Body)
-    if err != nil {
-        log.Println(err)
-    }
-    log.Println(string(body))
-    var t test_struct
-    err = json.Unmarshal(body, &t)
-    if err != nil {
-        log.Println(err)
-    }
-    log.Println(t.Test)
-    fmt.Fprintf(rw,"OK\n")
+func packerJson(rw http.ResponseWriter, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(string(body))
+	var t test_struct
+	err = json.Unmarshal(body, &t)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(t.Test)
+	fmt.Fprintf(rw, "OK\n")
+}
+
+func packerPost(rw http.ResponseWriter, req *http.Request) {
+	log.Println(req)
+	if req.Method == "GET" {
+		fmt.Fprintf(rw, "wrong method")
+	} else {
+		log.Println(req)
+		req.ParseForm()
+		// logic part of log in
+		fmt.Println("fname:", req.PostFormValue("fname"))
+		fmt.Fprintf(rw, "OK\n")
+	}
 }
 
 func main() {
-    http.HandleFunc("/packer", packer)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/post", packerPost)
+	http.HandleFunc("/json", packerJson)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
