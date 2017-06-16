90ac181f7691be00d3b23c84f8dad473a0ac0514
Uzhinskiy Boris <lspci@mail.ru>
2017-06-17 00:41:05 +0300

    .

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ type variables_struct struct {
 	host_name          string
 }
 
-func packerJson(rw http.ResponseWriter, req *http.Request) {
+func packerJson1(rw http.ResponseWriter, req *http.Request) {
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
 		log.Println(err)
@@ -35,14 +35,27 @@ func packerJson(rw http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(rw, "OK\n")
 }
 
+func packerJson(rw http.ResponseWriter, req *http.Request) {
+	log.Println(req)
+	var vars variables_struct
+
+	decoder := json.NewDecoder(req.Body)
+	err := decoder.Decode(&vars)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(vars.host_name)
+	fmt.Fprintf(rw, "OK\n")
+}
+
 func packerPost(rw http.ResponseWriter, req *http.Request) {
+	log.Println(req)
 	if req.Method == "GET" {
 		fmt.Fprintf(rw, "wrong method")
 	} else {
-		log.Println(req)
 		req.ParseForm()
 		// logic part of log in
-		fmt.Println("fname:", req.PostFormValue("fname"))
+		fmt.Println("fname:", req.PostForm["fname"])
 		fmt.Fprintf(rw, "OK\n")
 	}
 }
